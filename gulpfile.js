cef302ace06aa85c46bc3ebb11f15a3fd66e3b15
Konrad Sroga <konradsroga@gmail.com>
2021-06-20 11:07:47 +0200

    Added babel

--- gulpfile.js
+++ gulpfile.js
@@ -2,17 +2,19 @@ const gulp = require('gulp');
 const concat = require('gulp-concat');
 const uglify = require('gulp-uglify');
 const minify = require('gulp-minify-css');
+const babel = require('gulp-babel');
 
 
 gulp.task('js', function() {
-    return gulp.src('src/js/*.js')
+    return gulp.src('src/js/index.js')
+        .pipe(babel({presets: ['@babel/env']}))
         .pipe(concat('FrostModals.min.js'))
         .pipe(uglify())
         .pipe(gulp.dest('src/js/'));
 });
 
 gulp.task('css', function() {
-    return gulp.src('src/css/*.css')
+    return gulp.src('src/css/style.css')
         .pipe(concat('FrostModals.min.css'))
         .pipe(minify())
         .pipe(gulp.dest('src/css/'));
